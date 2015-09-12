5bfbfb44ca5cca1a86e79066a27e4eef0727efd7
Sunil Unka <sunil.unka@gmail.com>
2015-09-12 19:44:12 -0400

    Finished first iteration and task of tumblr_mailer

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -2,19 +2,32 @@ var fs = require('fs');
 
 
 
-
 function csvParse(csvFile){
-  
-  function readCSVFile(file){
-    var csvFile = fs.readFileSync(file);
 
-    return csvFile;
+  var contacts = [];
+
+  function ContactPerson(firstName, lastName, lastContact, email){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.numMonthsSinceContact = lastContact;
+    this.emailAddress = email;
   }
 
+  function readCSVFile(file){
+    var csvFile = fs.readFileSync(file, { encoding: "utf8"});
+    var lines = csvFile.split("\n").slice(1, -1);
 
-  console.log(readCSVFile(csvFile));
+    for(var i = 0; i < lines.length; i++){
+      var details = lines[i].split(",");
+      contacts.push(new ContactPerson(details[0], details[1], details[2], details[3]));
+    }
+    return;  
+  }
 
+  readCSVFile(csvFile);
+  console.log(contacts);
+  return contacts;
 
 }
 
-csvParse('./friends_list.csv');
+csvParse('./friend_list.csv');
